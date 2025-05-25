1606ed6a4e39964b3ef6bbd7cede55b37f5d9807
kdmch <mitori.kadomachi@gmail.com>
2025-05-25 16:45:27 +0900

    added error.vue

---
+++ error.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="error-wrapper">
+    <MtrmHeader />
+    <div class="error-container">
+      <div class="error-code">{{ error.statusCode }}</div>
+      <div class="error-details">{{ error.message }}</div>
+      <NuxtLink to="/" class="error-details">もどる</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const error = useError()
+</script>
+
+<style scoped>
+.error-wrapper {
+  width: 100vw;
+  height: 100svh;
+  display: flex;
+  flex-flow: column nowrap;
+}
+.error-container {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  max-width: 320px;
+  margin: auto;
+  text-align: center;
+  flex-grow: 1;
+}
+.error-code {
+  font-size: 60px;
+  font-weight: bold;
+}
+</style>
